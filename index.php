341ba8cd4dff856313b314aad7da639109cc11f3
Abdyushev Rafail <Abdyushev.R@romir.ru>
2024-05-03 17:54:35 +0300

    index

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Pictures</title>
+        <link href="galleria/img_tool/style.css" rel="stylesheet" type="text/css">
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.js"></script>
+        <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/galleria/1.5.7/galleria.min.js"></script> -->
+
+        <link rel="stylesheet" href="galleria/img_tool/jquery.iviewer.css" />
+        <script type="text/javascript" src="galleria/img_tool/jquery.js" ></script>
+        <script type="text/javascript" src="galleria/img_tool/jqueryui.js" ></script>
+        <script type="text/javascript" src="galleria/img_tool/jquery.mousewheel.min.js" ></script>
+        <script type="text/javascript" src="galleria/img_tool/jquery.iviewer.js" ></script>
+        <script src="galleria/galleria-1.5.1.js"></script>
+    </head>
+    <body>
+        <div class="galleria">
+            <img id="pic1" src="img/IMG_6418.PNG" data-big="img/berezy.jpg"> 
+            <img id="pic2" src="img/repin_krest_hod.jpg" data-big="img/night.png">
+            <img id="pic3" src="img/savr.jpg" data-big="img/step.jpg">
+        </div>
+        <div id="myModal" class="myModal">
+            <span id="close-modal" class="close">&times;</span>
+            <div class="modal-content-vis" id="viewer1"></div>
+        </div>
+        <script>
+        </script>
+        <!-- <script src="galleria/galleria-1.5.1.js"></script> -->
+        <!-- <script src="galleria/check.js"></script> -->
+</body>
+</html>
