12203b742b155b60066603f53dc6017fe9e8d179
Abdyushev Rafail <Abdyushev.R@romir.ru>
2024-05-17 11:33:49 +0300

    checking canvas

--- galleria/main2.js
+++ galleria/main2.js
@@ -15,38 +15,57 @@ function createID(i) {
     return namID;
 }
 
+function isCanvasBlank(canvas) {
+  return !canvas.getContext('2d', { willReadFrequently: true })
+            .getImageData(0, 0, canvas.width, canvas.height).data
+            .some(channel => channel !== 0);
+}
+
 document.getElementById('arrow').onclick = () => {
-    document.body.style.cursor = "crosshair";
-    let id = createID(1);
-    let obj = new SelectUrgent(id, "rnd_box");
-    document.onmousedown = obj.mousedown.bind(obj);
+    if (!(isCanvasBlank(document.getElementById("myCanvas")))) {
+        document.body.style.cursor = "crosshair";
+        let id = createID(1);
+        let obj = new SelectUrgent(id, "rnd_box");
+        document.onmousedown = obj.mousedown.bind(obj);
+    } else {
+        alert("Сначала необходимо создать область!");
+    }
+    
 }
 
 document.getElementById('reset').onclick = () => {
-    let canvas = document.getElementById("myCanvas");
-    canvas.classList.add("no-vis");
-    // sel_box.remove();
-    let sel_box = document.getElementById("sel_box");
-    sel_box.remove();
-    let rnd_box = document.querySelectorAll(".rnd_box");
-    rnd_box.forEach(el => {
-        el.remove();
-    });
-    document.getElementById("test_box").style.display = "none";
+    if (!(isCanvasBlank(document.getElementById("myCanvas")))) {
+        let canvas = document.getElementById("myCanvas");
+        canvas.classList.add("no-vis");
+        // sel_box.remove();
+        let sel_box = document.getElementById("sel_box");
+        sel_box.remove();
+        let rnd_box = document.querySelectorAll(".rnd_box");
+        rnd_box.forEach(el => {
+            el.remove();
+        });
+        document.getElementById("test_box").style.display = "none";
+    } else {
+        alert("Сначала необходимо создать область!");
+    }
+    
 }
 
 document.getElementById('save').onclick = () => {
-    let canvas = document.getElementById('myCanvas');
+    if (!(isCanvasBlank(document.getElementById("myCanvas")))) {
+        let canvas = document.getElementById('myCanvas');
+        canvas.toBlob(function(blob) {
 
+            let link = document.createElement('a');
+            link.download = 'example.png';
+            link.href = URL.createObjectURL(blob);
+            link.click();
+            URL.revokeObjectURL(link.href);
+          }, 'image/png');
+    } else {
+        alert("Сначала необходимо создать область!");
+    }
     
-    canvas.toBlob(function(blob) {
-
-        let link = document.createElement('a');
-        link.download = 'example.png';
-        link.href = URL.createObjectURL(blob);
-        link.click();
-        URL.revokeObjectURL(link.href);
-      }, 'image/png');
 }
 
 
