b6c5f0237001d42938424d14027eb3ead956c576
Abdyushev Rafail <Abdyushev.R@romir.ru>
2024-09-26 16:53:33 +0300

    showBigPic.php

--- showBigPic.php
+++ showBigPic.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Pictures</title>
-    <link href="galleria/img_tool/style.css" rel="stylesheet" type="text/css">
+    <link href="galleria/img_tool/style_edit.css" rel="stylesheet" type="text/css">
     <link rel="stylesheet" href="galleria/img_tool/jquery.iviewer.css" />
     <script type="text/javascript" src="galleria/img_tool/jquery.js" ></script>
     <script type="text/javascript" src="galleria/img_tool/jqueryui.js" ></script>
@@ -15,8 +15,19 @@
     </style>
 </head>
 <body>
+    <div id="test_box">
+        <canvas id="myCanvas" class="class-Canvas"></canvas>
+    </div>
     <div id="myModal" class="myModal">
-            <div class="modal-content-vis" id="viewer1"></div>
+            <div id="viewer1" class="my-viewer"></div>
+    </div>
+    <div class="buttons-wrapper">
+        <button type="button" id="crop">Вырезать</button>
+        <button type="button" id="arrow">Обводка</button>
+        <button type="button" id="reset">Reset</button>
+        <button type="button" id="save">Save</button>
+        <input type="checkbox" id="color-check" name="color-check" value="color-check">
+        <input type="color" id="color" name="color" value="#000000">
     </div>
     <?php
        
@@ -37,8 +48,8 @@
                     jQuery(myFunc);
         }
         mainFunc(picSrc);
-        console.log(picSrc);
     </script>
+    <script src="galleria/main2.js" type="module"></script>
 </body>
 </html>
 
