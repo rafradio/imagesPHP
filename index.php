286f19fca82a927d9cf15f672adf3cd1bcdd73c2
Abdyushev Rafail <Abdyushev.R@romir.ru>
2024-09-26 16:53:06 +0300

    index.php

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Pictures</title>
-        <link href="galleria/img_tool/style.css" rel="stylesheet" type="text/css">
+        <link href="galleria/img_tool/style_edit.css" rel="stylesheet" type="text/css">
         <script src="http://ajax.googleapis.com/ajax/libs/jquery/1/jquery.js"></script>
         <!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/galleria/1.5.7/galleria.min.js"></script> -->
 
@@ -17,17 +17,15 @@
     </head>
     <body>
         <div class="galleria">
-            <img id="pic1" src="img/IMG_6418.PNG" data-big="img/berezy.jpg"> 
-            <img id="pic2" src="img/repin_krest_hod.jpg" data-big="img/night.png">
-            <img id="pic3" src="img/savr.jpg" data-big="img/step.jpg">
+            <img id="pic1" src="img/IMG_6418.PNG" data-big="img/megafon1.jpg"> 
+            <img id="pic2" src="img/repin_krest_hod.jpg" data-big="img/megafon2.jpg">
+            <img id="pic3" src="img/savr.jpg" data-big="img/megafon3.jpg">
         </div>
         <div id="myModal" class="myModal">
             <span id="close-modal" class="close">&times;</span>
-            <div class="modal-content-vis" id="viewer1"></div>
+            <div class="my-viewer" id="viewer1"></div>
         </div>
-        <script>
-        </script>
         <!-- <script src="galleria/galleria-1.5.1.js"></script> -->
-        <!-- <script src="galleria/check.js"></script> -->
-</body>
+        
+    </body>
 </html>
